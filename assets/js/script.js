cab92039125d95f0f0418553b7af8cf30e375683
Gavin Aldred <Gavin.Aldred@nationaltimbergroup.com>
2023-02-01 20:59:55 +0000

    pushes base variables needed. Adds required date format to picker and saves value to a variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
+// required variables 
+
+let latitude = ""
+let longtitude = ""
+let date = ""
+let city = ""
+const googleAPIKey = "AIzaSyCoIJTitaSal9kU_w5Gz0c-M5epiS69i44"
+
+// base ajax calls to see repsponse 
+$.ajax({
+  url: "https://nominatim.openstreetmap.org/search.php?city=taipei&format=jsonv2", // uses city as the search
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
+
+$.ajax({
+  url: "https://api.sunrisesunset.io/json?lat=38.907192&lng=-77.036873&timezone=UTC&date=today", // with long and lat
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
+
+
+
 var timeDisplayEl = $("#time-display");
 
 // handle displaying the time
@@ -11,9 +36,14 @@ $(function () {
     $('#datepicker').datepicker({
       changeMonth: true,
       changeYear: true,
+      dateFormat: 'yy-mm-dd'
     });
   });
 
+  // saves the date to the date variable 
+  $('#datepicker').on('change', function() {
+    date = $(this).val();
+  });
 
 
 
